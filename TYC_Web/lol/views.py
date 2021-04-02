82ae391f2c065a8c16635979a338767259e8a825
Benoolean <benxiao778@gmail.com>
2021-04-02 15:16:17 -0400

    Updating to latest Django

--- TYC_Web/lol/views.py
+++ TYC_Web/lol/views.py
@@ -1,16 +1,27 @@
 from django.views import generic
-from django.views import View
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
 from .models import TournamentModel
 
+
 @method_decorator(login_required, name='dispatch')
-class LeagueIndexView(View):
+class LeagueIndexView(DetailView):
     model = TournamentModel
+    template_name = 'lol/index.html'
+
+    queryset = model.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'tournament_list' : self.queryset}) 
+
+
+@method_decorator(login_required, name='dispatch')
+class RiotAccountView(LeagueIndexView):
+    model = RiotAccountModel
     template_name = 'lol/index.html'
 
     queryset = model.objects.all()
